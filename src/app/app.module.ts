49c1109eae5e7ed7b5c5538346cdbab39c780855
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-16 11:26:26 -0400

    created main components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainInterfaceComponent } from './main-interface/main-interface.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainInterfaceComponent,
+    LeftPanelComponent,
+    MapViewComponent,
+    NavBarComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule,
